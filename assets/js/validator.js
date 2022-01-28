29126e70ee34761b5be09a43fa7cb1a2902dec6f
bernardbdas <bernarddas1@gmail.com>
2022-01-29 04:28:12 +0530

    commit

--- assets/js/validator.js
+++ assets/js/validator.js
@@ -3,36 +3,40 @@ function validate() {
 }
 //fetchingIDs
 //const form = document.getElementById('form');
-var uname = document.getElementById('name');
-var email = document.getElementById('email');
-var phn = document.getElementById('phone');
-var pass = document.getElementById('password');
-var cpass = document.getElementById('cnf-pass');
-var span = document.getElementsByTagName('span');
+let uname = document.getElementById('name');
+let email = document.getElementById('email');
+let phn = document.getElementById('phone');
+let pass = document.getElementById('password');
+let cpass = document.getElementById('cnf-pass');
+let span = document.getElementsByTagName('span');
 
-//fetching values
-var unameVal = uname.value.trim();
-var emailVal = email.value.trim().toLowerCase();
-var phnVal = phn.value.trim();
-var passVal = pass.value.trim();
-var cpassVal = cpass.value.trim();
-
+//locking numeric chars
+//invalidChars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+//uname.addEventListener("keydown", function() {
+//    if (invalidChars.includes(uname.value)) {
+//        uname.preventDefault();
+//    }
+//});
 
 //validate username
+function locknums(evt) {
+    const nums = /[^a-zA-Z]/;
+    var inp = String.fromCharCode(evt.which);
+    if (nums.test(inp)) {
+        evt.preventDefault();
+        span[0].innerText = "username CANNOT contain numbers or special characters";
+        span[0].style.color = "red";
+    }
+}
 uname.onkeydown = function() {
-    const nums = /[a-zA-Z]/;
-    if (unameVal === "") {
+
+    if (uname.value === "") {
         span[0].innerText = "username cannot be blank";
         span[0].style.color = "red";
     } else
-    if (unameVal.length < 3) {
+    if (uname.value.length < 3) {
         span[0].innerText = "username must contain atleast 3 characters";
         span[0].style.color = "red";
-    } else
-    if (nums.test(unameVal)) {
-        span[0].innerText = "username must NOT contain numbers";
-        span[0].style.color = "red";
-
     } else {
         span[0].innerText = "Valid username";
         span[0].style.color = "lime";
@@ -41,16 +45,23 @@ uname.onkeydown = function() {
 
 //validate email
 email.onkeydown = function() {
-    const validMail1 = /[a-z0-9\.-]@amazon.in/;
-    const validMail2 = /[a-z0-9\.-]@amazon.com/;
-    if (validMail1.test(emailVal) || validMail2.test(emailVal)) {
-        span[0].innerText = "Valid Email Address";
-        span[0].style.color = "lime";
+    //const validMail1 = /^([\.\_a-zA-Z0-9]+)@([a-zA-Z]+)\.([a-zA-Z]){2,8}$/;
+    //const validMail2 = /^([\.\_a-zA-Z0-9]+)@([a-zA-Z]+)\.([a-zA-Z]){2,3}\.[a-zA-Z]{1,3}$/;
+    const validMail1 = /^([\.\_a-zA-Z0-9]+)@cdpl.in$/;
+    const validMail2 = /^([\.\_a-zA-Z0-9]+)@cdpl.com$/;
+    const validMail3 = /^([\.\_a-zA-Z0-9]+)@cdpl.co.in$/;
+    if (validMail1.test(email.value) || validMail2.test(email.value) || validMail3.test(email.value)) {
+        span[1].innerText = "Valid Email Address";
+        span[1].style.color = "lime";
+    } else if (email.value === "") {
+        span[1].innerText = "email cannot be blank";
+        span[1].style.color = "red";
     } else {
-        span[0].innerText = "Invalid Email Address";
-        span[0].style.color = "red";
+        span[1].innerText = "Invalid Email Address";
+        span[1].style.color = "red";
     }
 }
+
 
 //validate phone number
 phn.onkeydown = function() {
